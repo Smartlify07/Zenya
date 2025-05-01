5d24b8d7edab1179fbc2bdf7b189d3802c8ae0b8
Obinna Anosike <smartlify09@gmail.com>
2025-05-02 00:59:05 +0100

    feat: build stat cards

---
+++ src/components/dashboard/stats-card.tsx
@@ -0,0 +1,67 @@
+import { Wallet } from 'lucide-react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+import { cn } from '@/lib/utils';
+
+type StatCardProps = {
+  title: string;
+  amount: number;
+  percentage: number;
+  type: 'balance' | 'income' | 'expense';
+};
+export const StatCard = ({
+  title,
+  amount,
+  percentage,
+  type,
+}: StatCardProps) => {
+  return (
+    <Card className="grid gap-2 font-inter rounded-md shadow-none col-span-1">
+      <CardHeader className="grid gap-2">
+        <div className="flex items-center justify-between">
+          <div
+            className={cn(
+              'rounded-md p-3 bg-neutral-50',
+              type === 'income' && 'text-purple-600',
+              type === 'expense' && 'text-red-600',
+              type === 'balance' && 'text-blue-600'
+            )}
+          >
+            <Wallet size={20} />
+          </div>
+        </div>
+        <CardTitle>
+          <h1 className="text-base font-semibold text-neutral-900">{title}</h1>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="grid gap-2 mt-1">
+        <h3 className="text-neutral-900 text-xl font-semibold">
+          ${amount.toLocaleString()}
+        </h3>
+      </CardContent>
+      <CardFooter className="grid gap-2">
+        <div className="flex items-center justify-between">
+          <p className="text-neutral-500 text-sm font-medium">
+            From last month
+          </p>
+          <p
+            className={cn(
+              'text-neutral-900 font-semibold',
+              type === 'income' && 'text-purple-600',
+              type === 'expense' && 'text-red-600',
+              type === 'balance' && 'text-blue-800'
+            )}
+          >
+            +{percentage}%
+          </p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { StatCard } from '@/components/dashboard/stats-card';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
@@ -5,5 +6,28 @@ export const Route = createFileRoute('/')({
 });
 
 function App() {
-  return <div></div>;
+  return (
+    <main className="grid gap-4 px-4 py-4">
+      <div className="grid gap-4 grid-cols-3">
+        <StatCard
+          type="balance"
+          title="Total Balance"
+          amount={60000}
+          percentage={30}
+        />
+        <StatCard
+          type="income"
+          title="Total Income"
+          amount={70000}
+          percentage={25}
+        />
+        <StatCard
+          type="expense"
+          title="Total Expense"
+          amount={10000}
+          percentage={30}
+        />
+      </div>
+    </main>
+  );
 }
