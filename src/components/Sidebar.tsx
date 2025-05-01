13e6139fc0d08a5e0423379a63cbd5ee61c17d23
Obinna Anosike <smartlify09@gmail.com>
2025-05-01 01:36:10 +0100

    chore: initialize project

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { Link } from '@tanstack/react-router';
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside className="flex flex-col gap-4">
+      <Link to="/"></Link>
+    </aside>
+  );
+};
+
+export default Sidebar;
