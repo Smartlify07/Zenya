26f7730826e9d744d0470dbc583ba59d8b8541df
Obinna Anosike <smartlify09@gmail.com>
2025-05-08 22:43:37 +0100

    chore: fix build issue

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { DashboardTopNav } from '@/components/dashboard/top-nav';
 import { AppSidebar } from '@/components/app-sidebar';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/sonner';
 import FinanceProvider from '@/context/FinanceProvider';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
