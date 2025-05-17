58c81cc6d9a86ea677ae96f9094d41d47e5fd481
Obinna Anosike <smartlify09@gmail.com>
2025-05-17 15:12:00 +0100

    fix build isssue

---
+++ src/components/pages/landing/cta.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button';
+import { useNavigate } from '@tanstack/react-router';
+
+export default function CallToAction() {
+  const navigate = useNavigate();
+  return (
+    <section className="flex flex-col items-center px-4 py-10 md:py-20 md:px-10 font-inter">
+      <div className="max-w-7xl flex flex-col w-full items-center gap-6">
+        <header className="flex flex-col gap-4 items-center md:w-10/12 lg:w-7/12">
+          <h1 className="text-2xl md:text-6xl text-center font-medium text-primary">
+            Ditch Spreadsheets, Track Smarter.
+          </h1>
+          <p className="text-center text-secondary-foreground">
+            Zenya helps you track income & expenses effortlessly, so you can
+            focus on what matters <span className="text-xl">-- your work.</span>
+          </p>
+        </header>
+        <Button
+          onClick={() => {
+            navigate({ to: '/signup' });
+          }}
+        >
+          Get Started
+        </Button>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/pages/landing/footer.tsx
@@ -0,0 +1,23 @@
+import { Logo } from '@/components/logo';
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="flex items-center py-10 font-inter justify-between px-4 md:px-10 gap-4">
+      <h1 className="flex items-center gap-2">
+        <Logo /> Zenya
+      </h1>
+
+      <ul className="flex items-center gap-2 text-neutral-700 text-sm">
+        <a href="https://x.com/Smartlify01" className="">
+          Contact us
+        </a>
+        <a href="/privacy">Privacy</a>
+      </ul>
+
+      <p className="text-secondary-foreground text-sm">
+        &copy; Zenya {currentYear}
+      </p>
+    </footer>
+  );
+}
