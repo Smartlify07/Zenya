3dced17d3d760f714579bdc8388c117691cbeea8
Obinna Anosike <smartlify09@gmail.com>
2025-05-17 15:15:37 +0100

    fix hero

--- src/components/pages/landing/hero.tsx
+++ src/components/pages/landing/hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <section className="flex pt-48 pb-20 px-4 md:px-0 justify-center flex-col items-center">
       <header className="flex flex-col md:w-10/12 lg:w-6/12 items-center gap-5">
-        <h1 className="text-3xl md:text-5xl font-medium text-primary text-center font-inter">
-          Most freelancers don’t track their finances, until it’s too late
+        <h1 className="text-3xl md:text-6xl font-medium text-primary text-center font-inter">
+          Ditch Spreadsheets, Track Smarter.
         </h1>
         <h3 className="md:text-lg font-inter text-secondary-foreground text-center font-normal">
           Zenya helps you stay on top of your income, expenses, and cashflow
