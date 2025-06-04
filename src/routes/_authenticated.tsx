e23183f747fc175c5cdfcb404eb2542e17ecc1eb
Obinna Anosike <smartlify09@gmail.com>
2025-06-04 07:51:07 +0100

    remove onboarding

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated')({
   component: RouteComponent,
-  beforeLoad: async ({ location }) => {
+  beforeLoad: async () => {
     const {
       data: { session },
       error,
@@ -14,21 +14,7 @@ export const Route = createFileRoute('/_authenticated')({
       console.log(session);
       throw redirect({ to: '/' });
     }
-    const { data: profile, error: profileError } = await supabase
-      .from('profiles')
-      .select('onboarding_complete')
-      .eq('id', session.user.id)
-      .maybeSingle();
 
-    if (
-      !profile?.onboarding_complete &&
-      location.pathname !== '/profile-setup'
-    ) {
-      throw redirect({ to: '/' });
-    }
-    if (profileError) {
-      throw new Error('Error occurred trying to get your profile');
-    }
     if (error) {
       console.error(error.message);
     }
